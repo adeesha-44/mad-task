16029aa24cf7df76795e255def5ebe77f4b70976
sheraniareesha86@gmail.com <sheraniareesha86@gmail.com>
2025-02-17 05:42:36 -0700

    first code

---
+++ mad.js
@@ -0,0 +1,18 @@
+let objArr=[
+    {
+        name:'Tehreem',
+        age: 19
+    },
+    {
+        name: 'Tania',
+        age: 20
+    },
+    {
+        name: 'Tehmoor',
+        age: 20
+    }
+];
+function print_data(data){
+    console.log(data);
+}
+objArr.map(print_data);
