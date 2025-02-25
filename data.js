aeb9bbca9ff1e2124ad67daca7c95f6b473c4253
sheraniareesha86@gmail.com <sheraniareesha86@gmail.com>
2025-02-25 01:23:04 -0700

    task

---
+++ data.js
@@ -0,0 +1,10 @@
+// data.js
+function getData() {
+    return [
+        { id: 1, name: "Emerald", type: "Gemstone" },
+        { id: 2, name: "Nipried", type: "Mineral" },
+        { id: 3, name: "Copper", type: "Metal" }
+    ];
+}
+
+module.exports = getData;
